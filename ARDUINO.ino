a661f920caa43a9bf71f7584280f3830e6459404
Luis Cardoso <163322910+LuisCardoso33@users.noreply.github.com>
2024-09-30 21:25:26 -0300

    Add files via upload

---
+++ ARDUINO.ino
@@ -0,0 +1,76 @@
+
+  int LED1 = 1;
+  int LED2 = 2;
+   int LED3 = 3;
+  int LED4 = 4;
+   int LED5 = 5;
+  int LED6 = 6;
+   int LED7 = 7;
+  int LED8 = 8;
+   int LED9 = 9;
+  int LED10 = 10;
+void setup() {
+     // put your setup code here, to run once:
+    pinMode(LED1,OUTPUT);
+    pinMode(LED2,OUTPUT);
+    pinMode(LED3,OUTPUT);
+    pinMode(LED4,OUTPUT);
+    pinMode(LED5,OUTPUT);
+    pinMode(LED6,OUTPUT);
+    pinMode(LED7,OUTPUT);
+    pinMode(LED8,OUTPUT);
+    pinMode(LED9,OUTPUT);
+    pinMode(LED10,OUTPUT);
+  }
+  
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED1,HIGH);
+   delay(50);
+  digitalWrite(LED2,HIGH);
+   delay(50);
+  digitalWrite(LED3,HIGH);
+   delay(50);
+  digitalWrite(LED4,HIGH);
+   delay(50);
+  digitalWrite(LED5,HIGH);
+   delay(50);
+  digitalWrite(LED6,HIGH);
+   delay(50);
+  digitalWrite(LED7,HIGH);
+   delay(50);
+  digitalWrite(LED8,HIGH);
+   delay(50);
+  digitalWrite(LED9,HIGH);
+   delay(50);
+  digitalWrite(LED10,HIGH);
+  delay(50);
+  //digitalWrite(LED2,LOW);
+  //digitalWrite(LED1,HIGH);
+  delay(500);
+  digitalWrite(LED10,LOW);
+   delay(50);
+  digitalWrite(LED9,LOW);
+   delay(50);
+  digitalWrite(LED8,LOW);
+   delay(50);
+  digitalWrite(LED7,LOW);
+   delay(50);
+  digitalWrite(LED6,LOW);
+   delay(50);
+  digitalWrite(LED5,LOW);
+   delay(50);
+  digitalWrite(LED4,LOW);
+   delay(50);
+  digitalWrite(LED3,LOW);
+   delay(50);
+  digitalWrite(LED2,LOW);
+   delay(50);
+  digitalWrite(LED1,LOW);
+   delay(50);
+
+
+}
